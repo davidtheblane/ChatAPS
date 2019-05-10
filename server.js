b76d4adadd9217b00902f28aa9e0981d42fd181c
davidtheblane <42559009+davidtheblane@users.noreply.github.com>
2019-05-09 22:34:17 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -8,19 +8,26 @@ var port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 let messages = []; 
+let users = [];
 
 app.get('/', function(req, res){
-  res.sendFile('/public/index.html', {root:'.'});
+  res.sendFile('index.html', {root:'.'});
+  //res.sendFile('/public/index.html', {root:'.'});
 });
 
 io.on('connection', function(socket){
     console.log(`Socket conectado: ${socket.id}`);
 
     socket.emit('previousMessages', messages);
-  callback();
 
-    socket.on('sendMessage', data => {
-        console.log("recebendo a mensagem do cliente")
+    socket.on('newUser', function(name){
+      users.push(name);
+      console.log(users);
+      socket.broadcast.emit('newUser', users)
+    })
+
+    socket.on('sendMessage', function(data){
+        console.log("recebendo a mensagem do cliente");
         messages.push(data);
         socket.broadcast.emit('receivedMessage', data);
     });
